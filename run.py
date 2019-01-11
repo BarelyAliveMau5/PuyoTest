756b56af4d7cc0609249d944a8b4a777d88c95aa
BarelyAliveMau5 <BarelyAliveMau5@users.noreply.github.com>
2019-01-11 15:55:56 -0200

    save my work, just in case this pc decides to die (as happened 1 week ago)

--- run.py
+++ run.py
@@ -1,47 +1,92 @@
 #!/usr/bin/env python3
 
-# This is a prototype/test/playground/toy project i've created out of curiosity to 
-# discover how to create a game like puyo-puyo, and also see how difficult it 
-# would be to create a super nice chain in the game. This was not supposed to 
+# This is a prototype/test/playground/toy project i've created out of curiosity
+# to discover how to create a game like puyo-puyo, and also see how difficult
+# it would be to create a super nice chain in the game. This was not supposed to
 # be a pretty copy of my work. This is literally just a bunch of code i wrote to 
 # test stuff.
 
 # I really want to implement a monte carlo search algorithm to generate 
-# efficient chains, instead of this crappy brute-force code implemented right now 
-# and i eventually will when i finish my college stuff. I dont plan to implement 
-# it using python in the future though (*cough* slow af *cough*). Maybe using  
-# the beloved Godot Engine? who knows :) (sure i will. i love you Godot <3)
+# efficient chains, instead of this crappy brute-force code implemented right
+# now and i eventually will when i finish my college stuff. I dont plan to
+# implement it using python in the future though (*cough* slow af *cough*).
+# Maybe using the beloved Godot Engine? who knows :)
+from typing import List, Tuple, Any
+from array import array
 
 zf = '.'  # zero fill, empty area, empty space
 big_group = 4  # minimum group size to be considered big enough to get removed
 
-types = "abcde"  # more types = greater complexity = harder to solve
+types = "abcde"  # more types -> greater complexity -> harder to solve
 
 sz_x = 6  # arena width
 sz_y = 12  # arena height
 
-# 1 dimension just sounds better..
-arena = [zf for i in range(sz_x * sz_y)]
+arena: List[int] = []
+
+
+class Board:
+    zf = 0
+    # todo: implement all board functions
+
+    def __init__(self, width: int, height: int, variations: int=5):
+        self._field: array = array('B', bytes([Board.zf] * width * height))
+        self._width = width
+        self._height = height
+        self._variations = variations
+
+    def resize(self, new_width, new_height):
+        self._field.fromlist([Board.zf for _ in range(new_width * new_height)])
+
+    def __iter__(self):
+        for y in range(self._height):
+            for x in range(self._width):
+                yield x, y
+
+    def at(self, x, y):
+        return self._field[x + y * self._width]
+
+    def pulldown(self):
+        for x in range(self._width):
+            # select only non-empty-space (non-zfs)
+            y_list = [self.at(x, y) for y in range(self._height)
+                      if self.at(x, y) != Board.zf]
+            # fill space "above" with empty-space, acts like gravity
+            y_list = [zf for _ in range(sz_y - len(y_list))] + y_list
+            # apply modifications
+            for y, v in enumerate(y_list):
+                set_at(x, y, v)
+
+
+# todo: implement game solver
+# todo: remove old code thats not inside classes
+
+def create_arena():
+    global arena
+    arena = [zf for _ in range(sz_x * sz_y)]
+
+
+create_arena()
 
 
 # translate 2d to 1d
-def at(x, y):
+def at(x: int, y: int):
     return arena[x + y * sz_x]
 
 
-def set_at(x, y, v):
+def set_at(x: int, y: int, v: Any):
     arena[x + y * sz_x] = v
 
 
 # store messages to print later
-msgs = []
+msgs: List[str] = []
 
 
 def s_print(*nargs, end='\n', sep=' '):
     msgs.append(sep.join(map(str, nargs)) + end)
 
 
-def arena_iter():
+def arena_iter() -> Tuple[int, int]:
     for y in range(sz_y):
         for x in range(sz_x):
             yield x, y
@@ -59,7 +104,7 @@ def pull_down():
             set_at(x, y, v)
 
 
-def __get_groups(x, y, groups):
+def __get_groups(x: int, y: int, groups):
     if not (sz_x > x >= 0 and sz_y > y >= 0):  # boundary error checking
         # this should never be true
         return
@@ -67,9 +112,9 @@ def __get_groups(x, y, groups):
     if group_type == zf:  # ignore empty space
         return
 
-    neighbors = []
+    neighbors: List[Tuple[int, int]] = []
 
-    point_queue = [(x, y)]
+    point_queue: List[Tuple[int, int]] = [(x, y)]
     # based on rosetta code's c++ flood-fill algorithm
     while point_queue:
         px, py = point_queue.pop(0)
@@ -86,7 +131,7 @@ def __get_groups(x, y, groups):
 
 
 def find_groups(debug=False):
-    groups = []  # [ [(x, y), (x, y)], ...]
+    groups: List[Tuple[int, int]] = []  # [ [(x, y), (x, y)], ...]
     for x, y in arena_iter():
         # avoid processing known groups
         if not [i for i in groups if (x, y) in i]:
@@ -114,13 +159,10 @@ def remove_big_groups(min_=big_group):
                 set_at(x, y, zf)
 
 
-def show(current_x=-1, current_y=-1, mark_char='x'):
+def show():
     for y in range(sz_y):
         for x in range(sz_x):
-            if current_x == x and current_y == y:
-                print(mark_char, end='')
-            else:
-                s_print(at(x, y), end='')
+            s_print(at(x, y), end='')
         s_print("")
     population = len([0 for x, y in arena_iter() if at(x, y) != zf])
     s_print("population:", population, "\n")
@@ -146,6 +188,7 @@ def main(max_pop):
     # the fact that i didn't optimize the code for performance. its a TEST
     from random import randint
     i = 0
+    smallest = sz_x * sz_y
     while True:
         msgs.clear()
         for x, y in arena_iter():
@@ -157,7 +200,9 @@ def main(max_pop):
         if pop < max_pop:
             print()
             break
-        print("\rsimulation: ", i, "last_pop:", pop, end='')  # i like to see how far we are
+        smallest = min(smallest, pop)
+        # i like to see how far we are
+        print("\rsimulation: ", i, "smallest:", smallest, end='')
         i += 1
     print(*msgs, sep='')
 
@@ -173,7 +218,7 @@ def simulate(values):
     msgs.clear()
     show()
     iter_game()
-    print(*msgs, sep=' ')
+    print(*msgs, sep='')
 
 
 def argument_parser():
@@ -182,16 +227,19 @@ def argument_parser():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=textwrap.dedent('''\
-             note:
-                custom sets are case-sensitive and must be 72 characters long. 
-                emojis can be used too if you like them (needs UTF-8 support)
+        note:
+            custom sets are case-sensitive and must be X*Y characters long 
+            when redefined with -d (default is 72, 6x12). 
+            emojis can be used too if you like them (needs UTF-8 support)
+            
+        example:
+            custom set:
+            run.py -c cbdeabdccabebbcbbcbdbbbdaaecceaeaaebadcdedbbdeabbadcd\
+deaecccdaeaaecdeaee
                 
-             example:
-                custom set:
-                run.py -c cbdeabdccabebbcbbcbdbbbdaaecceaeaaebadcdedbbdeabbadcddeaecccdaeaaecdeaee
-                
-                custom set with emoji:
-                run.py -z ".." -c 🍰👀😈😡😀👀😈🍰🍰😀👀😡👀👀🍰👀👀🍰👀😈👀👀👀😈😀😀😡🍰🍰😡😀😡😀😀😡👀😀😈🍰😈😡😈👀👀😈😡😀👀👀😀😈🍰😈😈😡😀😡🍰🍰🍰😈😀😡😀😀😡🍰😈😡😀😡😡
+            custom set with emoji:
+            run.py -z ".." -c 🍰👀😈😡😀👀😈🍰🍰😀👀😡👀👀🍰👀👀🍰👀😈👀👀👀😈😀😀😡🍰🍰😡😀😡😀😀😡👀😀😈🍰😈😡😈👀👀😈😡\
+😀👀👀😀😈🍰😈😈😡😀😡🍰🍰🍰😈😀😡😀😀😡🍰😈😡😀😡😡
              '''))
 
     group = parser.add_mutually_exclusive_group()
@@ -204,7 +252,9 @@ def argument_parser():
     parser.add_argument("-z", help="empty space character(s)", default=".",
                         metavar="CHARS", action="store")
     parser.add_argument("-x", help="define the characters of the random set",
-                        metavar="CHARS", default="abcde", action="store")
+                        metavar="CHARS", action="store")
+    parser.add_argument("-d", help="arena dimension", metavar=("X", "Y"),
+                        action="store", nargs=2, type=int)
     return parser
 
 
@@ -217,9 +267,12 @@ if __name__ == "__main__":
         types = args.x
     if args.z:
         zf = args.z
+    if args.d:
+        sz_x, sz_y = args.d
+        create_arena()
     if args.c:
-        if len(args.c) != 72:
-            print("custom sets must be 72 characters long.")
+        if len(args.c) != sz_x * sz_y:
+            print(f"custom sets must be {sz_x * sz_y} characters long.")
             exit(-1)
         simulate(args.c)
     elif args.s:
